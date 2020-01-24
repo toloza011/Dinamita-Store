9f0c1f48a89218098897915cecd360d655a4fbd2
manuel sepulevda <manux.rayxxd@gmail.com>
2020-01-24 16:56:01 -0300

    layout

--- app/Http/Controllers/tablaController.php
+++ app/Http/Controllers/tablaController.php
@@ -7,8 +7,6 @@ use Illuminate\Http\Request;
 class tablaController extends Controller
 {
     public function inicio(){
-        $numero =  prueba::all();
-       //dd($numero[0]['a']);
-        return view('fotos',compact('numero'));
+        return view('layout',compact('numero'));
     }
 }
