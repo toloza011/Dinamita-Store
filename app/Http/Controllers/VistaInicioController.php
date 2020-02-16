4d053d732bce2f207b086ed3dca5d9be06acdf0b
RAYXXD <manuel.sepulveda1701@alumnos.ubiobio.cl>
2020-02-16 16:45:12 -0300

    manejo de sesiones

--- app/Http/Controllers/VistaInicioController.php
+++ app/Http/Controllers/VistaInicioController.php
@@ -17,16 +17,35 @@ class VistaInicioController extends Controller
         $InfoPlataforma = Plataforma::all();
         $InfoUser = DB::select("SELECT users.id, users.name, users.email FROM users WHERE users.id = '$idusuario' ");
         
+        $nameUser = $InfoUser[0]->name;
+        session(['identificador' => $idusuario]);
+        session(['nombre' => $nameUser]);
+        
+     /*    return $request->session()->all(); */
 
-        return view('inicio', compact('InfoUser', 'InfoPlataforma', 'InfoCategoria'));
+        return view('inicio', compact('InfoUser', 'InfoPlataforma', 'InfoCategoria','request')); 
+        
     }
 
     function registrar(){
         $InfoCategoria = Categoria::all();
         $InfoPlataforma = Plataforma::all();
         
-        $InfoUser = null;
+        $InfoUser = DB::select("SELECT users.id, users.name, users.email FROM users WHERE users.id = '$idusuario' ");
        // dd($InfoUser);
         return view('registro',compact('InfoCategoria','InfoPlataforma','InfoUser'));
+    }
+
+    function vistajuego(Request $request){
+       
+        $InfoCategoria = Categoria::all();
+        $InfoPlataforma = Plataforma::all();
+        //dd($request);
+        $request=$request;
+       
+       
+        
+       // dd($InfoUser);
+        return view('juegos',compact('InfoCategoria','InfoPlataforma','request'));
     }
 }
