12f2b5a77a4eff698cc406222ec51e8b6e70529a
Esteban <estebantoloza1998mail.com>
2020-02-17 18:18:29 -0300

    vista registro

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,7 +11,7 @@
 		body {
 			font-family: 'Montserrat', sans-serif;
     /* Otro ejemplo */
-		}	
+		}
 	</style>
 	    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700|Roboto:300,400,500,600,700">
         <link rel="shortcut icon" href="/assets/media/logos/favicon-web.png" />
@@ -33,13 +33,13 @@
 		</div>
 		<br>
 		<div class="row justify-content-center">
-		
+
 		@if(Session::has('mensaje'))
 			    <div class="alert alert-danger col-12 col-md-4"><em> {!! session('mensaje') !!}</em></div>
 				@endif
 		</div>
-				
-			       
+
+
 				<div class="kt-login__signin">
 					<div class="kt-login__head">
 						<h5 class="kt-login__title" style="text-align:center;"><strong>Iniciar Sesión</strong></h5>
@@ -48,8 +48,8 @@
 					<form class="kt-form" method="POST" action="validacion" >
 
 						<div class="row justify-content-center">
-						
-						
+
+
 							<div class="col-12 col-md-4">
 									<div class="input-group {{$errors->has('email')?'alert alert-danger':''}}">
 										<input class="form-control" type="email" style="border-radius:15px" placeholder="correo" name="email" value="{{old('email')}}"autocomplete="off" required>
@@ -58,10 +58,10 @@
 									<br>
 									<div class="input-group {{$errors->has('password')?'alert alert-danger':''}}">
 										<input class="form-control" type="password" style="border-radius:15px" placeholder="Contraseña" name="password" required>
-                                        
+
 									</div>
 									<br>
-										
+
 									</div>
 									<input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
 										<!--
@@ -70,19 +70,19 @@
 										</div>-->
 									</div>
 									<div class="row justify-content-center">
-									
-										
+
+
 											<input type="submit" id="kt_login_signin_submit" class="btn btn-danger form-control col-6 col-md-2" style="border-radius:25px" value="Acceder">
 											<!--<button id="kt_login_signin_submit" class="btn btn-brand btn-pill kt-login__btn-primary">Acceder</button>-->
-										
-								
+
+
 							</div>
 
-								
+
 						</div>
 					</form>
 							<br><br><em><strong><h3 align="center" style="color: red">"Los mejores juegos al mejor precio"</h3></strong></em>
-							</div>	
+							</div>
 
 
 
