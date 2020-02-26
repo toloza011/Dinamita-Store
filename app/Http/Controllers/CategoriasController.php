40ec2953b016aa2f01107fa896944f39c930d9bf
manuel sepulevda <manux.rayxxd@gmail.com>
2020-02-25 22:00:17 -0300

    esteban klo

---
+++ app/Http/Controllers/CategoriasController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Categoria;
+use Illuminate\Http\Request;
+use DataTables;
+use App\Plataforma;
+use DB;
+
+class CategoriasController extends Controller
+{
+    public function getCategoriaAll()
+    {
+        $categorias =  Categoria::select("id_categoria", "nombre_categoria");
+   
+        return Datatables::of($categorias)
+        ->addColumn('action', function ($id_categoria) {
+            return '<a href="/categoria/' . $id_categoria->id_categoria . '/editar" class="btn btn-dark"><i class="fa fa-edit"></i></a></button>';
+        })
+
+        ->addColumn('action2', function ($id_categoria) {
+            $token =  csrf_field();
+
+            return ' <form action="QuitarCategoria" method="post" id="FormularioEliminarCategoria" class="delete">  '.$token.' <input type="hidden" name="id_categoria" id="id_categoria" value='.  $id_categoria->id_categoria  .'>
+            <td><button type="submit" value="Eliminar" id="boton1" class="btn btn-danger" onclick="return confirm(`¿Está seguro que desea eliminar?`);" /><i class="fa fa-trash"></i></td>
+            </form>
+            '; 
+
+            
+            })
+          
+        ->rawColumns(['action2' => 'action2','action' => 'action'])
+        ->make(true);
+    }
+
+    public function edit(Request $request,$id_categoria){
+
+        $InfoCategoria = Categoria::all();
+        $InfoPlataformaJ = Plataforma::select('plataformas.id_plataforma', 'plataformas.nombre_plataforma')->join('juegos', 'plataformas.id_plataforma', '=', 'juegos.id_plataforma')->groupBy('id_plataforma', 'nombre_plataforma')->get();
+        $InfoPlataformaS = Plataforma::select('plataformas.id_plataforma', 'plataformas.nombre_plataforma')->join('subscripciones', 'plataformas.id_plataforma', '=', 'subscripciones.id_plataforma')->groupBy('id_plataforma', 'nombre_plataforma')->get();
+
+
+        $nombre_categoria = DB::table('categorias')->select("*")->where('id_categoria','=',$id_categoria)->first();
+
+
+
+        return view('categoria.editar',compact('nombre_categoria','InfoCategoria','InfoPlataformaJ','InfoPlataformaS','request'));
+
+    }
+
+    public function update($id_categoria){
+     
+        $nuevoNombre = $_GET['nombre'];
+        DB::table('categorias')->where("id_categoria","=",$id_categoria)->update(['nombre_categoria' => $nuevoNombre]);
+     
+		return redirect()->route('agregar')->with('mensaje', 'Categoria Modificado con exito');
+
+    }
+
+
+    public function QuitarCategoria(Request $request){
+
+        $id_categoria = $_POST['id_categoria'];
+        
+
+		DB::table('categorias')
+
+        ->where('id_categoria', '=',$id_categoria)
+        ->delete();
+
+		
+        \Session::flash('mensaje','Categoria eliminado con exito');
+
+
+        return redirect()->route('agregar');
+
+
+    }
+
+    
+
+}
---
+++ resources/views/categoria/editar.blade.php
@@ -0,0 +1,65 @@
+@extends('layout')
+@section('content')
+
+<div class="kt-portlet">
+    <div class="kt-portlet__head">
+        <div class="kt-portlet__head-label">
+            <h3 class="kt-portlet__head-title">
+                Registro de Categoria
+            </h3>
+        </div>
+    </div>
+
+    <div class="row">
+        <form action="{{route('update', $nombre_categoria->id_categoria )}}" method="get">
+            <div class="col-md-6 ">
+
+                <div class="kt-portlet__body">
+
+                    <div class="form-group {{ $errors->has('nombre_categoria') ? ' has-error' : '' }}">
+                        <label>Nombre</label>
+                        <input type="text" class="form-control" placeholder="Ingrese nombre" id="nombre" name="nombre" value="{{$nombre_categoria->nombre_categoria}}">
+                        @if ($errors->has('$nombre_categoria'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('$nombre_categoria') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+
+
+
+
+                </div>
+
+
+
+            </div>
+
+
+            <div class="row" align="center">
+                <div class="col-md-8">
+                    <div class="form-group">
+
+
+                        <input style="margin-top:45px" type="submit" value="Modificar Categoria" class="btn btn-dark" id="caja">
+
+                    </div>
+                </div>
+            </div>
+        </form>
+
+    </div>
+</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+<script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+
+
+
+
+
+
+
+@endsection
